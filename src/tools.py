5b574fa12aed8c2418de7a0ee8455d0c20d1ba36
mrbertp <aps795@gmail.com>
2021-09-26 14:26:37 +0200

    calculation of individual metrics and basic plotting

--- src/tools.py
+++ src/tools.py
@@ -1,57 +1,16 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import datetime as dt
 
 fillable_names = ['CULTIVO', 'TAMAÑO', 'LOSA', 'GOLPE']
 fillable_units = ['cubos', 'cm2', 'semillas/cubo']
-metrics_units = ['semillas', 'plantas', 'plantas/semilla', 'semillas/planta', 'cm2', 'semillas/cm2', 'cm2/semilla', 'plantas/cubo', 'plantas/cm2', 'cm2/planta']
-metrics_names = ['SIEMBRA', 'PROGRENIE', 'FERTILIDAD', 'INVERSION', 'AREA', 'DENSIDAD', 'OCUPACION', 'COLONIZACION', 'RENDIMIENTO', 'HABITAT']
+collective_units = ['semillas', 'plantas', 'plantas/semilla', 'semillas/planta', 'cm2', 'semillas/cm2', 'cm2/semilla', 'plantas/cubo', 'plantas/cm2', 'cm2/planta']
+collective_names = ['SIEMBRA', 'PROGENIE', 'FERTILIDAD', 'INVERSION', 'AREA', 'DENSIDAD', 'OCUPACION', 'COLONIZACION', 'RENDIMIENTO', 'HABITAT']
 
-def process_sheet(filename):
-	
-	wb = load_workbook('data/' + filename)
-	sheets = wb.sheetnames
-
-	for sheet in sheets:
-		ws = wb[sheet]
-		#ws.title = ws.title + ' ' + ws['O2'].value
-
-		# read data
-		seedlings = []
-		for row in range(3,13):
-			for col in range(3,13):
-				letter = get_column_letter(col)
-				seedlings.append(ws[letter + str(row)].value)
-		seedlings = np.array(seedlings)
-
-		fillable_data = []
-		for row in range(2,6):
-			fillable_data.append(ws['O' + str(row)].value)
-
-		fillable = dict(zip(fillable_names,fillable_data))
-
-		# calculation of metrics
-		metrics = {}
-
-		metrics['SIEMBRA'] = fillable['GOLPE']*fillable['TAMAÑO']
-		metrics['PROGRENIE'] = np.sum(seedlings)
-		metrics['FERTILIDAD'] = metrics['PROGRENIE'] / metrics['SIEMBRA']
-		metrics['INVERSION'] =  metrics['SIEMBRA'] / metrics['PROGRENIE']
-		metrics['AREA'] = fillable['TAMAÑO'] * fillable['LOSA']
-		metrics['DENSIDAD'] = metrics['SIEMBRA'] / metrics['AREA']
-		metrics['OCUPACION'] = metrics['AREA'] / metrics['SIEMBRA']
-		metrics['COLONIZACION'] = metrics['PROGRENIE'] / fillable['TAMAÑO']
-		metrics['RENDIMIENTO'] = metrics['PROGRENIE'] / metrics['AREA']
-		metrics['HABITAT'] = metrics['AREA'] / metrics['PROGRENIE']
-
-		# write data into excel sheet
-		for row,value in zip(range(2,12),metrics.values()):
-			ws['S' + str(row)] = value
-
-	wb.save('data/' + filename)
-
-def write_sheet(parameters):
+def create_sheet(parameters):
 
 	wb = Workbook()
 	ws = wb.active
@@ -77,14 +36,66 @@ def write_sheet(parameters):
 		ws[get_column_letter(6+ncol) + str(row)] = fillable_units[row-3]
 
 	for row in range(2,12):
-		ws[get_column_letter(8+ncol) + str(row)] = metrics_names[row-3]
+		ws[get_column_letter(8+ncol) + str(row)] = collective_names[row-2]
 
 	for row in range(2,12):
-		ws[get_column_letter(10+ncol) + str(row)] = metrics_units[row-2]
+		ws[get_column_letter(10+ncol) + str(row)] = collective_units[row-2]
 
 	ws[get_column_letter(5+ncol) + '2'] = crop
 	ws[get_column_letter(5+ncol) + '3'] = nrow * ncol
 
 	wb.save(f'data/{crop}_{date}_{nrow}x{ncol}.xlsx')
 
-write_sheet(('cebolla',3,7))
+def process_sheet(filename):
+	
+	wb = load_workbook('data/' + filename)
+	ws = wb.active
+
+	nrow, ncol = list(map(int,filename.split('.')[0].split('_')[-1].split('x')))
+
+	# read data
+	individual = {}
+	aux = []
+	for row in range(3,3+nrow):
+		for col in range(3,3+ncol):
+			letter = get_column_letter(col)
+			aux.append(ws[letter + str(row)].value)
+	individual['PROGENIE'] = np.array(aux)
+
+	fillable_data = []
+	for row in range(2,6):
+		fillable_data.append(ws[chr(ord('A')+(4+ncol)) + str(row)].value)
+
+	fillable = dict(zip(fillable_names,fillable_data))
+
+	# calculation of metrics
+	individual['FERTILIDAD'] = individual['PROGENIE'] / fillable['GOLPE']
+	individual['INVERSION'] = np.reciprocal(individual['FERTILIDAD'])
+	individual['RENDIMIENTO'] = individual['PROGENIE'] / fillable['LOSA']
+	individual['HABITAT'] = np.reciprocal(individual['RENDIMIENTO'])
+
+	collective = {}
+	collective['SIEMBRA'] = fillable['GOLPE']*fillable['TAMAÑO']
+	collective['PROGENIE'] = np.sum(individual['PROGENIE'])
+	collective['FERTILIDAD'] = collective['PROGENIE'] / collective['SIEMBRA']
+	collective['INVERSION'] =  collective['SIEMBRA'] / collective['PROGENIE']
+	collective['AREA'] = fillable['TAMAÑO'] * fillable['LOSA']
+	collective['DENSIDAD'] = collective['SIEMBRA'] / collective['AREA']
+	collective['OCUPACION'] = collective['AREA'] / collective['SIEMBRA']
+	collective['COLONIZACION'] = collective['PROGENIE'] / fillable['TAMAÑO']
+	collective['RENDIMIENTO'] = collective['PROGENIE'] / collective['AREA']
+	collective['HABITAT'] = collective['AREA'] / collective['PROGENIE']
+
+	# write data into excel sheet
+	for row,value in zip(range(2,12),collective.values()):
+		ws[chr(ord('A')+(8+ncol)) + str(row)] = value
+
+	wb.save('data/' + filename)
+
+	sns.violinplot(data=individual['HABITAT'])
+	sns.swarmplot(data=individual['HABITAT'], color='white')
+
+	plt.show()
+
+#create_sheet(('pea',10,10))
+process_sheet('pea_2021-09-26_10x10.xlsx')
