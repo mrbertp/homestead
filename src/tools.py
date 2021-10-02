06b66c202fb06e749fc9830888f3c5f3ffaa113c
mrbertp <aps795@gmail.com>
2021-10-02 02:45:57 +0200

    formatless name, file auto-organization, recording to database, dataframe structure

--- src/tools.py
+++ src/tools.py
@@ -1,14 +1,16 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
+import os
 
 fillable_names = ['CULTIVO', 'TAMAÑO', 'LOSA', 'GOLPE']
 fillable_units = ['cubos', 'cm2', 'semillas/cubo']
-collective_units = ['semillas', 'plantas', 'plantas/semilla', 'semillas/planta', 'cm2', 'semillas/cm2', 'cm2/semilla', 'plantas/cubo', 'plantas/cm2', 'cm2/planta']
 collective_names = ['SIEMBRA', 'PROGENIE', 'FERTILIDAD', 'INVERSION', 'AREA', 'DENSIDAD', 'OCUPACION', 'COLONIZACION', 'RENDIMIENTO', 'HABITAT']
+collective_units = ['semillas', 'plantas', 'plantas/semilla', 'semillas/planta', 'cm2', 'semillas/cm2', 'cm2/semilla', 'plantas/cubo', 'plantas/cm2', 'cm2/planta']
 
 def create_sheet(parameters):
 
@@ -44,14 +46,16 @@ def create_sheet(parameters):
 	ws[get_column_letter(5+ncol) + '2'] = crop
 	ws[get_column_letter(5+ncol) + '3'] = nrow * ncol
 
-	wb.save(f'data/{crop}_{date}_{nrow}x{ncol}.xlsx')
+	wb.save(f'../dat/templates/{crop}_{date}_{nrow}x{ncol}.xlsx')
 
 def process_sheet(filename):
 	
-	wb = load_workbook('data/' + filename)
+	wb = load_workbook('../dat/templates/' + filename + '.xlsx')
 	ws = wb.active
 
-	nrow, ncol = list(map(int,filename.split('.')[0].split('_')[-1].split('x')))
+	name = filename.split('_')[0]
+	date = filename.split('_')[1]
+	nrow, ncol = list(map(int,filename.split('_')[2].split('x')))
 
 	# read data
 	individual = {}
@@ -86,16 +90,52 @@ def process_sheet(filename):
 	collective['RENDIMIENTO'] = collective['PROGENIE'] / collective['AREA']
 	collective['HABITAT'] = collective['AREA'] / collective['PROGENIE']
 
+	# write data into database
+	with open('../dat/database.csv', 'a') as file:
+		
+		file.write(name)
+		file.write('\t')
+		file.write(date)
+		file.write('\t')
+		
+		for i in range(len(collective.values())):
+			file.write(str(np.around(list(collective.values())[i],2)))
+			if i != len(collective.values())-1:
+				file.write('\t')
+
+		file.write('\n')
+
 	# write data into excel sheet
 	for row,value in zip(range(2,12),collective.values()):
 		ws[chr(ord('A')+(8+ncol)) + str(row)] = value
 
-	wb.save('data/' + filename)
+	wb.save('../dat/filled/' + filename + '.xlsx')
+	os.remove('../dat/templates/' + filename + '.xlsx')
 
-	sns.violinplot(data=individual['HABITAT'])
-	sns.swarmplot(data=individual['HABITAT'], color='white')
+def update_database(path):
 
-	plt.show()
+	with open('../dat/database.csv', 'w') as file:
 
-#create_sheet(('pea',10,10))
-process_sheet('pea_2021-09-26_10x10.xlsx')
+		file.write('CUL')
+		file.write('\t')
+		file.write('FEC')
+		file.write('\t')
+
+		for i in range(len(collective_names)):
+			file.write(collective_names[i])
+			if i != len(collective_names)-1:
+				file.write('\t')
+
+		file.write('\n')
+
+	files = os.listdir(path)
+
+	for file in files:
+		process_sheet(file[:-5])
+
+#create_sheet(('wheat',8,8))
+#process_sheet('wheat_2021-10-01_8x8')
+update_database('../dat/templates/')
+
+df = pd.read_csv('../dat/database.csv', sep='\t')
+print(df)
